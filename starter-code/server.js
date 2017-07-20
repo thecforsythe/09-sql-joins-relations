ebe5d55e2f36007e72d45b1bae7c9cd5036492dc
Chuck Forsythe <thecforsythe@gmail.com>
2017-07-20 13:50:54 -0500

    line 26 inner joined author and article tables

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = '';// TODO: Don't forget to set your own conString
+const conString = 'postgres://localhost:5432/kilovolt';// TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
@@ -24,7 +24,12 @@ app.get('/new', function(request, response) {
 app.get('/articles', function(request, response) {
   // REVIEW: This query will join the data together from our tables and send it back to the client.
   // TODO: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
-  client.query(``)
+  client.query(`SELECT *
+    FROM articles
+    INNER JOIN author
+    ON author.author_id = articles.author_id`)
+
+
   .then(function(result) {
     response.send(result.rows);
   })
